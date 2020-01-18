70827c5f2f03197d4800614bcb38068441608dbe
David Shah <dave@ds0.me>
2020-01-18 15:36:58 +0000

    Misc fixes
    Signed-off-by: David Shah <dave@ds0.me>

--- ecp5-partial/make_connection.py
+++ ecp5-partial/make_connection.py
@@ -1,3 +1,5 @@
+for nname, net in ctx.nets:
+	ctx.lockNetRouting(nname)
 ctx.cells["slice_i"].addInput("A0")
 ctx.connectPort("ctr[26]", "slice_i", "A0")
 ctx.cells["slice_i"].setParam("LUT0_INITVAL", "0101010101010101") # LED is active low so invert
