05ff788eadfc26423d453641932cc2ebc973368f
David Shah <dave@ds0.me>
2020-01-18 14:43:55 +0000

    Fix for Python changes
    Signed-off-by: David Shah <dave@ds0.me>

--- ecp5-partial/make_connection.py
+++ ecp5-partial/make_connection.py
@@ -1,3 +1,4 @@
-ctx.makeConnection("ctr[26]", "slice_i", "A0")
+ctx.cells["slice_i"].addInput("A0")
+ctx.connectPort("ctr[26]", "slice_i", "A0")
 ctx.cells["slice_i"].setParam("LUT0_INITVAL", "0101010101010101") # LED is active low so invert
 ctx.cells["slice_i"].setParam("A0MUX", "A0") # remove constant mux on LUT input
